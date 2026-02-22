7630840cdd61e6c1d4e5b602fa9b575151344d4b
Ameco-dev <psnw.adrien@gmail.com>
2026-02-22 16:23:35 +0100

    feat: initialize database on app startup

--- cmd/zentxt/main.go
+++ cmd/zentxt/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-    "log"
-    "github.com/gofiber/fiber/v3"
+	"log"
+
+	"github.com/Adriusops/zentxt/internal/storage"
+	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
+
+	// Initialize the database connection
+	_, err := storage.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Initialize a new Fiber app
-    app := fiber.New()
+	app := fiber.New()
 
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
-    })
+	// Define a route for the GET method on the root path '/'
+	app.Get("/", func(c fiber.Ctx) error {
+		// Send a string response to the client
+		return c.SendString("Hello, World 👋!")
+	})
 
-    // Start the server on port 3000
-    log.Fatal(app.Listen(":3000"))
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
 }
